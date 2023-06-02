0892d45a27d02ffd0507a0c8327ec761cb041fdd
Fany Martinez <estefania.marhen@gmail.com>
2023-06-02 09:57:45 +0100

    ADD KYUs
    7-8

--- 7-kyu/7-kyu.js
+++ 7-kyu/7-kyu.js
@@ -1,90 +1,90 @@
+function getAVG(arrayOfNumbers) {
+ if (!Array.isArray(arrayOfNumbers) || arrayOfNumbers.length === 0) {
+   return 0;
+  }
+   let sum = 0;
+   const arrayOfRealNumbers = arrayOfNumbers.filter((number) => typeof number === 'number');
+ arrayOfRealNumbers.forEach((number) => {
+     sum += number;
+   });
+ return sum / arrayOfRealNumbers.length;
+  }
 
-function getAVG(){
-    function getAVG(arrayOfNumbers) {
+  function replaceVocalWithPosition(phrase) {
+    if (typeof phrase !== "string") {
+      return null;
+    }
+    const vocals = ['a', 'e', 'i', 'o', 'u'];
+    let phraseFormated = '';
 
-        if (!Array.isArray(arrayOfNumbers)) {
-      
-          return 0;
-      
-        }
-      
-        if (arrayOfNumbers.length === 0) {
-      
-          return 0;
-      
-        }
-      
-      
-      
-        let avg;
-      
-        let sumNumbers = 0;
-      
-        let countNumbers = 0;
-      
-      
-      
-        for (const number of arrayOfNumbers) {
-      
-          if (typeof number === 'number') {
-      
-            sumNumbers += number;
-      
-            countNumbers++;
-      
-          }
-      
-        }
-      
-        avg = sumNumbers / countNumbers;
-      
-      
-      
-        return avg;
-      
+    for (let i = 0; i < phrase.length; i++) {
+      if (vocals.includes(phrase[i].toLowerCase())) {
+        phraseFormated += i + 1;
+      } else {
+        phraseFormated += phrase[i];
       }
-      
-      
-      
-      function getAVGAdvanced(arrayOfNumbers) {
-      
-        if (!Array.isArray(arrayOfNumbers) || arrayOfNumbers.length === 0) {
-      
-          return 0;
-      
+    }
+    return phraseFormated;
+  }
+
+  function convertArrayStringsToArrayNumbers(arrayOfStrings) {
+    if (!Array.isArray(arrayOfStrings)) {
+      return [0];
+    }
+    const arrayOfNumbers = [];
+    for (const string of arrayOfStrings) {
+      let number = 0;
+      if (typeof string === 'string') {
+        const numberOfString = parseInt(string);
+        if (!isNaN(numberOfString)) 
+        {number = numberOfString;
         }
-      
-        let sum = 0;
-      
-      
-      
-        const arrayOfRealNumbers = arrayOfNumbers.filter((number) => typeof number === 'number');
-      
-        arrayOfRealNumbers.forEach((number) => {
-      
-          sum += number;
-      
-        });
-      
-      
-      
-        return sum / arrayOfRealNumbers.length;
-      
       }
-}
+      arrayOfNumbers.push(number);
+    }
+    return arrayOfNumbers;
+  }
 
+  function removeValues(array1, array2) {
+    if (!Array.isArray(array1)) { 
+      return [];
+    }
+    if (!Array.isArray(array2)) { 
+      return array1;
+    }
+    return array1.filter(element => (!array2.includes(element)));
+  }
 
+  function findDuplicates(array) {
+    if (!Array.isArray(array)) {
+      let emptyArray = [];
+      return emptyArray;
+    }
+  
+    const uniqueItems = [];
+    const duplicatedItems = [];
+    array.forEach((element) => {
+  
+      if (!uniqueItems.includes(element)) {
+        uniqueItems.push(element);
+      } else if (!duplicatedItems.includes(element)){
+        duplicatedItems.push(element);
+      }
+    })
+    return duplicatedItems;
+  
+  }
 
-
-
-
-
-
-
-
-
-
+  function convertNumberToStringWords(number) {
+    let numberToString
+    return stringConcat
+  } 
 
 module.exports = {
-getAVG,
+  getAVG,
+  replaceVocalWithPosition,
+  convertArrayStringsToArrayNumbers,
+  removeValues,
+  findDuplicates,
+  convertNumberToStringWords
 }
